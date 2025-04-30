741d511ea588941c332878755eb92e28c16e72bc
Alan <ale@kea.dk>
2025-04-30 17:35:59 +0200

    fetch and show

---
+++ js/liste.js
@@ -0,0 +1,18 @@
+const url = "https://kea-alt-del.dk/t7/api/products";
+
+fetch(url).then(response=>response.json()).then(show);
+
+function show(data){
+    console.log(data);
+    const main = document.querySelector("main");
+data.forEach(product => {
+    const newArticle = document.createElement("article");
+    const newH2 = document.createElement("h2");
+    newH2.textContent=product.productdisplayname;
+    newArticle.appendChild(newH2); 
+    const newImg = document.createElement("img");
+    newImg.src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+    newArticle.appendChild(newImg); 
+    main.appendChild(newArticle)
+});
+}
