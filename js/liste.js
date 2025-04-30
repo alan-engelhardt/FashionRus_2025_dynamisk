cf7c5df92a184e1f587651a1ba89e0e1c1d7ac84
Alan <ale@kea.dk>
2025-04-30 19:20:50 +0200

    add onSale

--- js/liste.js
+++ js/liste.js
@@ -6,14 +6,14 @@ function show(data){
 console.log(data);
 const main = document.querySelector("main");
 const markup = data.map(product =>`
- <article class="smallProduct">
+ <article class=${product.discount ? "onSale" : "class=smallProduct"} >
             <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product image" />
             <h3>${product.productdisplayname}</h3>
             <p class="subtle">Tshirts | Nike</p>
             <p class="price">DKK <span>${product.price}</span>,-</p>
-            <div class="discounted">
+            <div class=discounted">
                 <p>Now DKK <span></span>,-</p>
-                <p><span></span>%</p>
+                <p><span>${product.discount}</span>%</p>
             </div>
             <a href="product.html?id=${product.id}">Read More</a>
         </article>
