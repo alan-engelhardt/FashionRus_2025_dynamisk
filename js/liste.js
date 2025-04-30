48bb1ef8741cb750b5b7a7b956527c873d1a050c
Alan <ale@kea.dk>
2025-04-30 17:37:45 +0200

    h3

--- js/liste.js
+++ js/liste.js
@@ -7,9 +7,9 @@ function show(data){
     const main = document.querySelector("main");
 data.forEach(product => {
     const newArticle = document.createElement("article");
-    const newH2 = document.createElement("h2");
-    newH2.textContent=product.productdisplayname;
-    newArticle.appendChild(newH2); 
+    const newH3 = document.createElement("h3");
+    newH3.textContent=product.productdisplayname;
+    newArticle.appendChild(newH3); 
     const newImg = document.createElement("img");
     newImg.src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
     newArticle.appendChild(newImg); 
