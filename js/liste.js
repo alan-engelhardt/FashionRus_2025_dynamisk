cafd9728601997a96675f98e04a5f0ca45273a86
Alan <ale@kea.dk>
2025-09-14 19:27:00 +0200

    beregn discount

--- js/liste.js
+++ js/liste.js
@@ -12,7 +12,7 @@ const markup = data.map(product =>`
             <p class="subtle">Tshirts | Nike</p>
             <p class="price">DKK <span>${product.price}</span>,-</p>
             <div class="discounted">
-                <p>Now DKK <span></span>,-</p>
+                <p>Now DKK <span>${Math.round(product.price - product.price * product.discount /100)}</span>,-</p>
                 <p><span>${product.discount}</span>%</p>
             </div>
             <a href="product.html?id=${product.id}">Read More</a>
