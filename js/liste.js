46fd300258eef8ef08280df05a5adc39eeb2105e
Alan <ale@kea.dk>
2025-09-22 23:08:17 +0200

    add category fetch

--- js/liste.js
+++ js/liste.js
@@ -1,4 +1,6 @@
-const url = "https://kea-alt-del.dk/t7/api/products";
+const category = new URLSearchParams(window.location.search).get("category");
+
+const url = "https://kea-alt-del.dk/t7/api/products?category=" + category;
 
 fetch(url).then(response => response.json()).then(show);
 
