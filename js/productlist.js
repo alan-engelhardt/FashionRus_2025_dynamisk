84a2b3d57d3bb9b001c1a23365bd23cd52390c3a
Alan <ale@kea.dk>
2025-09-29 08:45:50 +0200

    add js mm

---
+++ js/productlist.js
@@ -0,0 +1,27 @@
+
+const productListContainer = document.querySelector("main");
+
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=20`)
+    .then((response) => response.json())
+    .then(data => showProducts(data))
+
+function showProducts(products) {
+    console.log(products);
+    products.forEach(element => {
+        console.log(element);
+        productListContainer.innerHTML += `<article class="smallProduct ${element.soldout && "soldOut"} ${element.discount && "onSale"}">
+            <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="product image" />
+            <h3>${element.productdisplayname}</h3>
+            <p class="subtle">Tshirts | Nike</p>
+            <p class="price">DKK <span>${element.price}</span>,-</p>
+            <div class="discounted">
+                <p>Now DKK <span>${Math.round(element.price - element.price * element.discount / 100)}</span>,-</p>
+                <p><span>${element.discount}</span>%</p>
+            </div>
+            <a href="product.html?id=${element.id}">Read More</a>
+        </article>`;
+    })
+
+}
+
+
