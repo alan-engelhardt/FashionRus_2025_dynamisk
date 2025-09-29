5564629c4f2ea4e6a35ea3295b7e03f40618a29b
Alan <ale@kea.dk>
2025-09-29 22:14:17 +0200

    concat allProducts

--- js/productlist.js
+++ js/productlist.js
@@ -1,22 +1,23 @@
 
 const productListContainer = document.querySelector("main");
-let allProducts;
+let allProducts = [];
 let startAt = 0;
 
 function getData(start) {
-    fetch(`https://kea-alt-del.dk/t7/api/products?limit=20&start=${start}`)
-        .then((response) => response.json())
-        .then(data => { allProducts = data; showProducts(data) })
+  fetch(`https://kea-alt-del.dk/t7/api/products?limit=10&start=${start}`)
+    .then((response) => response.json())
+    .then(data => { allProducts = allProducts.concat(data); showProducts(allProducts) })
 }
 
 getData(startAt);
 
 document.querySelector("#showMoreButton").addEventListener("click", showMore);
+document.querySelector("#toTop").addEventListener("click", () => { document.documentElement.scrollTop = 0; });
 
 function showMore() {
-    startAt += 20;
-    getData(startAt);
-    document.documentElement.scrollTop = 0;
+  startAt += 10;
+  getData(startAt);
+  //document.documentElement.scrollTop = 0;
 }
 
 const knapper = document.querySelectorAll("#filters button")
@@ -25,25 +26,25 @@ knapper.forEach(knap => knap.addEventListener("click", filterData));
 const numItems = document.querySelector("#items");
 
 function filterData() {
-    console.log(this.dataset.gender)
-    knapper.forEach(knap => knap.classList.remove("aktivtFilter"));
-    if (this.dataset.gender == "all") {
-        showProducts(allProducts);
-    } else {
-        const udsnit = allProducts.filter(product => product.gender == this.dataset.gender);
-        showProducts(udsnit);
-        console.log(udsnit);
-    }
-    numItems.textContent += " (" + this.dataset.gender + ")";
-    this.classList.add("aktivtFilter");
+  console.log(this.dataset.gender)
+  knapper.forEach(knap => knap.classList.remove("aktivtFilter"));
+  if (this.dataset.gender == "all") {
+    showProducts(allProducts);
+  } else {
+    const udsnit = allProducts.filter(product => product.gender == this.dataset.gender);
+    showProducts(udsnit);
+    console.log(udsnit);
+  }
+  numItems.textContent += " (" + this.dataset.gender + ")";
+  this.classList.add("aktivtFilter");
 }
 
 function showProducts(products) {
-    numItems.textContent = products.length;
-    productListContainer.innerHTML = "";
-    products.forEach(element => {
-        console.log(element);
-        productListContainer.innerHTML += `<article class="smallProduct ${element.soldout && "soldOut"} ${element.discount && "onSale"}">
+  numItems.textContent = products.length;
+  productListContainer.innerHTML = "";
+  products.forEach(element => {
+    // console.log(element);
+    productListContainer.innerHTML += `<article class="smallProduct ${element.soldout && "soldOut"} ${element.discount && "onSale"}">
             <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="product image" />
             <h3>${element.productdisplayname}</h3>
             <p class="subtle">Tshirts | Nike</p>
@@ -54,7 +55,7 @@ function showProducts(products) {
             </div>
             <a href="product.html?id=${element.id}">Read More</a>
         </article>`;
-    })
+  })
 
 }
 
