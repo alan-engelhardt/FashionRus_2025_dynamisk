66f9e64ea02ec36d72ee02d0bfb94527f36c4eb9
TroelsAtWork <tras@kea.dk>
2025-09-30 15:02:25 +0200

    Add event delegation

--- js/productlist.js
+++ js/productlist.js
@@ -4,49 +4,46 @@ document.querySelector("h2").textContent = category;
 
 const productListContainer = document.querySelector("main");
 
-document.querySelectorAll("#filters button").forEach(knap => knap.addEventListener("click", showFiltered));
+document.querySelector("#filters").addEventListener("click", showFiltered);
 
-function showFiltered() {
-    console.log(this.dataset.gender);
-    const gender = this.dataset.gender;
-    if (gender == "All") {
-        showProducts(allData);
-    } else {
-        const udsnit = allData.filter(product => product.gender == gender);
-        showProducts(udsnit);
-    }
+function showFiltered(event) {
+  const gender = event.target.dataset.gender;
+  if (gender == "All") {
+    showProducts(allData);
+  } else {
+    const udsnit = allData.filter((product) => product.gender == gender);
+    showProducts(udsnit);
+  }
 }
 
 let allData;
 
 fetch(`https://kea-alt-del.dk/t7/api/products?limit=30&category=${category}`)
-    .then((response) => response.json())
-    .then((data) => {
-        allData = data;
-        showProducts(allData);
-    });
-
-
+  .then((response) => response.json())
+  .then((data) => {
+    allData = data;
+    showProducts(allData);
+  });
 
 function showProducts(products) {
-    console.log(products);
-    productListContainer.innerHTML = "";
-    products.forEach((element) => {
-        console.log(element);
-        productListContainer.innerHTML += `<article class="smallProduct ${element.soldout && "soldOut"
-            } ${element.discount && "onSale"}">
-            <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id
+  productListContainer.innerHTML = "";
+  products.forEach((element) => {
+    productListContainer.innerHTML += `<article class="smallProduct ${
+      element.soldout && "soldOut"
+    } ${element.discount && "onSale"}">
+            <img src="https://kea-alt-del.dk/t7/images/webp/640/${
+              element.id
             }.webp" alt="product image" />
             <h3>${element.productdisplayname}</h3>
             <p class="subtle">Tshirts | Nike</p>
             <p class="price">DKK <span>${element.price}</span>,-</p>
             <div class="discounted">
                 <p>Now DKK <span>${Math.round(
-                element.price - (element.price * element.discount) / 100
-            )}</span>,-</p>
+                  element.price - (element.price * element.discount) / 100
+                )}</span>,-</p>
                 <p><span>${element.discount}</span>%</p>
             </div>
             <a href="product.html?id=${element.id}">Read More</a>
         </article>`;
-    });
+  });
 }
