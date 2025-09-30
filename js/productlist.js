dd6689729f504b4e479ea0094af8dabd2a102bcc
Alan <ale@kea.dk>
2025-09-30 12:01:20 +0200

    add h2 category

--- js/productlist.js
+++ js/productlist.js
@@ -1,32 +1,31 @@
 const params = new URLSearchParams(window.location.search);
 const category = params.get("category");
+document.querySelector("h2").textContent = category;
 
 const productListContainer = document.querySelector("main");
 
 fetch(`https://kea-alt-del.dk/t7/api/products?limit=20&category=${category}`)
-  .then((response) => response.json())
-  .then((data) => showProducts(data));
+    .then((response) => response.json())
+    .then((data) => showProducts(data));
 
 function showProducts(products) {
-  console.log(products);
-  products.forEach((element) => {
-    console.log(element);
-    productListContainer.innerHTML += `<article class="smallProduct ${
-      element.soldout && "soldOut"
-    } ${element.discount && "onSale"}">
-            <img src="https://kea-alt-del.dk/t7/images/webp/640/${
-              element.id
+    console.log(products);
+    products.forEach((element) => {
+        console.log(element);
+        productListContainer.innerHTML += `<article class="smallProduct ${element.soldout && "soldOut"
+            } ${element.discount && "onSale"}">
+            <img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id
             }.webp" alt="product image" />
             <h3>${element.productdisplayname}</h3>
             <p class="subtle">Tshirts | Nike</p>
             <p class="price">DKK <span>${element.price}</span>,-</p>
             <div class="discounted">
                 <p>Now DKK <span>${Math.round(
-                  element.price - (element.price * element.discount) / 100
-                )}</span>,-</p>
+                element.price - (element.price * element.discount) / 100
+            )}</span>,-</p>
                 <p><span>${element.discount}</span>%</p>
             </div>
             <a href="product.html?id=${element.id}">Read More</a>
         </article>`;
-  });
+    });
 }
